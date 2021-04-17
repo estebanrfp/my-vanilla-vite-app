aa0171c9f016c83eac41a95e4121d559a236b40d
estebanrfp <estebanrfp@gmail.com>
2021-04-17 21:18:56 +0200

    add main.js as entry point

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
+import { registerSW } from 'virtual:pwa-register'
+
+import './lib/index'
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // show a prompt to user
+    console.log('New content available, click on reload button to update.')
+
+  },
+  onOfflineReady() {
+    console.log('show a ready to work offline to user')
+    // show a ready to work offline to user
+  }
+})
+
+updateSW()
